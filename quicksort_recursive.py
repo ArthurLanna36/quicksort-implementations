78e49e310a2ca450a9ebb3644f1dd672b2cae33b
Arthur <arthur.galanna36@gmail.com>
2024-10-22 12:22:31 -0300

    Recursive quicksort with swaps and comparisons counting

--- quicksort_recursive.py
+++ quicksort_recursive.py
@@ -1,36 +1,19 @@
-# Partition function
-def partition(array, low, high):
-    # Choose the pivot
-    pivot = array[high]
-
-    # Index of smaller element and indicates
-    # the right position of pivot found so far
-    i = low - 1
-
-    # Traverse array[low...high] and move all smaller
-    # elements to the left side. Elements from low to
-    # i are smaller after every iteration
-    for j in range(low, high):
-        if array[j] < pivot:
-            i += 1
-            swap(array, i, j)
-
-    # Move pivot after smaller elements and
-    # return its position
-    swap(array, i + 1, high)
-    return i + 1
-
-# Swap function
-def swap(array, i, j):
-    array[i], array[j] = array[j], array[i]
-
-# The QuickSort function implementation
-def quicksort_recursive(array, low, high):
+def quicksort_recursive(arr, low, high, comparisons=0, swaps=0):
     if low < high:
-        # pivot_index is the partition return index of pivot
-        pivot_index = partition(array, low, high)
+        pivot_index, comparisons, swaps = partition(arr, low, high, comparisons, swaps)
+        comparisons, swaps = quicksort_recursive(arr, low, pivot_index - 1, comparisons, swaps)
+        comparisons, swaps = quicksort_recursive(arr, pivot_index + 1, high, comparisons, swaps)
+    return comparisons, swaps
 
-        # Recursion calls for smaller elements
-        # and greater or equals elements
-        quicksort_recursive(array, low, pivot_index - 1)
-        quicksort_recursive(array, pivot_index + 1, high)
+def partition(arr, low, high, comparisons, swaps):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        comparisons += 1
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+            swaps += 1
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    swaps += 1
+    return i + 1, comparisons, swaps
